8134050d10a8d0dc7dd256e423f49a1cf3afd213
Kenn Pascascio <kenn370@gmail.com>
2018-07-11 10:50:08 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+require('dotenv').config();
+const express = require('express'); 
+const app = express(); 
+
+app.listen(process.env.PORT, () => console.log(`server is listening on port ${process.env.PORT}`));
