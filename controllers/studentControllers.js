fbefad71b95560fbf7e41a60a76158a5848d4878
Evan Sondgerath <esondgerath@gmail.com>
2018-07-18 16:51:32 -0400

    Studentcontrollers fix

--- controllers/studentControllers.js
+++ controllers/studentControllers.js
@@ -1,47 +1,57 @@
-var router = require('express').Router();
+var express = require('express')
+var router = express.Router()
+var Student = require('../services/studentServices');
 
 
-    
-router.put(`/api/studentaccount/:id`, function(req, res) {
-    var data = req.params.id;
-    var student = req.body.student
-    
-    student
-        .update({ 
-            first_name : student.first_name,
-            last_name : student.last_name,
-            email : student.email,
-            password : student.password,
-            resume : student.resume,
-            social_media_id : student.social_media_id,
-        },
-        {where: {id: data}}
-        ).then(
-            function updateSuccess(updatedLog) {
-                res.json({
-                    student: student
-                });            
-            },
-            function updateError(err){
-                res.send(500, err.message);
-            }
-        )
-});
+router.get('/students' , function(req, res) {
+
+    Student
+    .getAll()
+    .then(
+      function findAllSuccess(data) {
+          res.json(data);
+      },
+      function findAllError(err) {
+          res.send(500, err.message);
+      }
+  );
+})
+
+router.put(`/api/studentaccount/:${id}`, function(req, res) {
+        var data = req.params.id;
+        var student = req.body.student
+        
+        Student.editStudent(data)
+            
+            .then(
+                function updateSuccess(updatedLog) {
+                    res.json({
+                        student: student
+                    });            
+                },
+                function updateError(err){
+                    res.send(500, err.message);
+                }
+            )
+    });
+
+router.post(`/api/studentaccount/:id`, function(req, res) {
+        var data = req.params.id;
+        var userid = req.student.id
+        Student.getOneStudent(data)
+            .then(
+                function findOneSuccess(data) {
+                    res.json(data);
+                },
+                function findOneError(err) {
+                    res.send(500, err.message);
+                }
+            );
+    });
 
 
-return router.get(`/api/studentaccount/:id`, function(req, res) {
-    var data = req.params.id;
-    var userid = req.user.id;
 
-    student
-        .findOne({
-            where: { id: data, owner: userid }
-        }).then(
-            function findOneSuccess(data) {
-                res.json(data);
-            },
-            function findOneError(err) {
-                res.send(500, err.message);
-            }
-        );
-});
+
+
+
+module.exports = router;
