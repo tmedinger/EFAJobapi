cbc5e7c2208ef13e495ebd4654219458855ae50c
masonseiler <masonseiler1@gmail.com>
2018-07-19 15:41:44 -0400

    backend update

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express'); 
 const app = express(); 
+require('./routes');
 
 app.listen(process.env.PORT, () => console.log(`server is listening on port ${process.env.PORT}`));
